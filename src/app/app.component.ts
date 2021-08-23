eb8d8deb706a4e80337a5e31e8dd4965bec83558
badrinath-dash <badrinath.dash@gmail.com>
2021-08-24 01:39:22 +1000

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+export class User {
+    public name: string;
+    public email: string;
+    public password: string;
+}
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+    model = new User();
+
+    Hobbies: string[] = [
+        'Acrobatics',
+        'Acting',
+        'Animation',
+        'Astronomy',
+        'Baking'
+    ];
+
+    constructor() { }
+
+    onSubmit(form) {
+        console.log(form.value)
+    }
+}
